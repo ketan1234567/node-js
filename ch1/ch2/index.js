96a35e8919829983f6d216bc0c98735a5defea2a
ketandeshmukh <deshmukhketan647@gmail.com>
2023-02-02 18:12:04 +0530

    first commit

---
+++ ch1/ch2/index.js
@@ -0,0 +1,6 @@
+//(function(exports, require, module, __filename, __dirname) {
+    // Module code 
+ //});
+
+console.log("This is second index Files");
+console.log(require);
---
+++ ch1/index.js
@@ -0,0 +1,5 @@
+console.log("This is node  we can code");
+var display=function(a,b){
+    return a+b;
+}
+display(100,200);
