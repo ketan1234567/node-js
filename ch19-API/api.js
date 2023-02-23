a7939b4390a0e0b3afdfeeb91ab2a26aa1c0eb7f
ketandeshmukh <deshmukhketan647@gmail.com>
2023-02-23 17:34:28 +0530

    "Yes"

--- ch19-API/api.js
+++ ch19-API/api.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const dbConnection=require('./mongodb');
+const mongoDB=require('mongodb');
 
 const app=express();
 // request method we can use when we wnat get data from database with help of postman
@@ -12,12 +13,34 @@ app.get('/',async (req,resp)=>{
    let data = await dbConnection();
     let result=await data.find().toArray();
    // console.log(result);
-    resp.send(result);
+    //resp.send(result);
 });
-app.post('/',(req,resp)=>{
-
-
-   console.log(req.body)
-   // resp.send({name:'anil'})
+app.post('/', async (req,resp)=>{
+   //console.log(req.body)
+   let data=  await dbConnection();
+  let result=await data.insertOne(req.body); // inset method retuns the promises 
+ console.log(result);
+resp.send(result)
 })
-app.listen(5000);
+
+app.put('/:name', async(req,resp)=>{
+let data= await dbConnection();
+let result=data.updateOne(
+   {name:req.params.name},
+   {$set:req.body}
+)  
+resp.send({result:"updated"})  // there are two object first is condition and second is code
+
+
+})
+
+app.delete("/:id",async (req,resp)=>{
+  // console.log(req.params.id)
+   let data=  await dbConnection();
+   let result= await data.deleteOne({
+      _id:new mongoDB.ObjectId(req.params.id)
+
+   })
+   resp.send(result);
+})
+app.listen(5000);       
